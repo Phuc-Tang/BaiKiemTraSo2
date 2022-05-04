e827ba3e6c5ff367e680eec0ce7d9c940a6fe27d
Phuc-Tang <ttphuc.20it6@vku.udn.vn>
2022-05-04 13:47:10 +0700

    Tang-Phuc

---
+++ app/src/main/java/com/example/crud_api/api/ApiClient.java
@@ -0,0 +1,21 @@
+package com.example.crud_api.api;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+// Kết nối với localhost
+    private static final String BASE_URL = "http://192.168.1.56/demo_studentManagerment/";
+     private static Retrofit retrofit;
+    public static Retrofit getApiClient(){
+        if (retrofit==null){
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
+
+
